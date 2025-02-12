4740e7b2aee8723ec84b3e48d976ac50f0bf57b0
hamza errahmouni <maradox6ken@gmail.com>
2025-02-13 00:46:23 +0100

    Footer-Header-Maincontent-react

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,13 +3,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      {/* إضافة مكون WelcomeMessage هنا */}
+      {/* إضافة مكون WelhcomeMessage هنا */}
       <WelcomeMessage /> 
 
       <div>
@@ -32,6 +36,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   );
 }
