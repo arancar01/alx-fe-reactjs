f5ada1d7572cf56dd714c153d139c2478ea067b0
hamza errahmouni <maradox6ken@gmail.com>
2025-02-18 19:09:35 +0100

    React Router settings

--- alx-react-app-props/src/main.jsx
+++ alx-react-app-props/src/main.jsx
@@ -1,9 +1,20 @@
+//import React from 'react';
+//import ReactDOM from 'react-dom/client';
+//import './index.css';
+//import App from './App.jsx';
+//
+//createRoot(document.getElementById('root')).render(
+//  <React.StrictMode>
+//    <App />
+//  </React.StrictMode>
+//);
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
+import App from './App';
+import './index.css';  // تضمين الأنماط العامة للتطبيق
 
-createRoot(document.getElementById('root')).render(
+// إضافة التطبيق داخل العنصر الذي يحمل id "root" في HTML
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
