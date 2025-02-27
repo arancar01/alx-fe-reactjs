337943382bcdd20f50771eb320fe3dcd040d28d1
hamza errahmouni <maradox6ken@gmail.com>
2025-02-27 03:02:16 +0000

    React Update zustand Store

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,20 +3,25 @@ import { create } from 'zustand';
 const useRecipeStore = create((set) => ({
   recipes: [],
 
+  // إضافة وصفة جديدة
   addRecipe: (newRecipe) => set((state) => ({
-    recipes: [...state.recipes, newRecipe],
+    recipes: [...state.recipes, newRecipe]
   })),
 
-  deleteRecipe: (recipeId) => set((state) => ({
-    recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
-  })),
-
+  // تحديث وصفة موجودة
   updateRecipe: (updatedRecipe) => set((state) => ({
     recipes: state.recipes.map((recipe) =>
       recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-    ),
+    )
   })),
+
+  // حذف وصفة
+  deleteRecipe: (recipeId) => set((state) => ({
+    recipes: state.recipes.filter((recipe) => recipe.id !== recipeId)
+  })),
+
+  // تعيين قائمة الوصفات (إضافة `setRecipes`)
+  setRecipes: (recipes) => set({ recipes }),
 }));
 
 export default useRecipeStore;
-
