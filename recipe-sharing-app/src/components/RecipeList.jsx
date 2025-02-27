d9877f8654c6dd72bf09072960090db9f78885a5
hamza errahmouni <maradox6ken@gmail.com>
2025-02-27 00:06:28 +0000

    React Recipe-sharing-app-React

---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,23 @@
+import useRecipeStore from '../store/recipeStore';
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <div>
+      <h2>Recipe List</h2>
+      {recipes.length > 0 ? (
+        recipes.map((recipe) => (
+          <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+          </div>
+        ))
+      ) : (
+        <p>No recipes available. Add some!</p>
+      )}
+    </div>
+  );
+};
+
+export default RecipeList;
