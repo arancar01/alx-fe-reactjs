4ac6f03598eb0d57d19762905f38b467e00f05c2
hamza errahmouni <maradox6ken@gmail.com>
2025-03-03 18:22:04 +0000

    PROFILE3

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,34 +1,21 @@
-// src/App.jsx
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-//import Home from './components/Home';
-//import Login from './components/Login';
+import Profile from './components/Profile';  // تأكد من وجود الملف
 import NotFound from './components/NotFound';
 import ProtectedRoute from './components/ProtectedRoute';
-import BlogPost from './components/BlogPost';  // تأكد من استيراد مكون BlogPost بشكل صحيح
-import Profile from './components/Profile';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="login" element={<Login />} />
-        
-        {/* مسار Profile محمي */}
         <Route 
-          path="profile" 
-          element={
-            <ProtectedRoute> 
-              <Profile />
-            </ProtectedRoute>
-          }
+          path="/profile/*" 
+          element={ 
+            <ProtectedRoute>  
+              <Profile /> 
+            </ProtectedRoute> 
+          } 
         />
-        
-        {/* مسار BlogPost ديناميكي */}
-        <Route path="/blog/:id" element={<BlogPost />} />  {/* هنا نضيف مسار ديناميكي للمقالات */}
-        
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
