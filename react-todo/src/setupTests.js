e299fea50e2f7300fbda3960c396ff58a32fb0d9
hamza_erra <maradox6ken@gmail.com>
2025-03-05 04:08:32 +0000

    Delete react-todo directory

--- react-todo/src/setupTests.js
+++
@@ -1,6 +0,0 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
-jest.mock('./logo.svg', () => 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zY3JlZW5zY2FwZSI+PHBhdGggZD0iTTE2MjEuNzg3IDYwOS4zODd6IiBmaWxsPSIjZTYxZGFmYiIvPjwvc3ZnPg==');
