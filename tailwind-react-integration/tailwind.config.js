986d7fa7f028afa18d9a7e00036003f558956ac4
hamza errahmouni <maradox6ken@gmail.com>
2025-03-10 03:15:16 +0000

    Tailwind 3.x

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,9 +1,7 @@
- /** @type {import('tailwindcss').Config} */
-export default {
-   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false,
-   theme: {
-     extend: {},
-   },
-   plugins: [],
- }
+  theme: { extend: {} },
+  variants: { extend: {} },
+  plugins: [],
+};
