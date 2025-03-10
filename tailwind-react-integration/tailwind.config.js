b816ebd9e26ac7ffe00715e5b140f4049a1de8a1
hamza errahmouni <maradox6ken@gmail.com>
2025-03-10 03:23:44 +0000

    Tailwind 3.x

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,7 +1,12 @@
+// eslint-disable-next-line
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false,
-  theme: { extend: {} },
-  variants: { extend: {} },
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // سيتم استخدام purge هنا
+  darkMode: false, // تعيين وضع الظلام
+  theme: {
+    extend: {},
+  },
+  variants: { // سيتم استخدام variants هنا
+    extend: {},
+  },
   plugins: [],
 };
