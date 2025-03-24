07dc6c2393014b610e1832043ce6ef19606a767c
hamza errahmouni <maradox6ken@gmail.com>
2025-03-24 05:05:35 +0000

    ............

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -17,7 +17,7 @@ const Search = () => {
       const data = await fetchUserData(username);
       setUserData(data);
     } catch {
-      setError("Looks like we can't find the user");
+      setError("Looks like we cant find the user");
     } finally {
       setLoading(false);
     }
