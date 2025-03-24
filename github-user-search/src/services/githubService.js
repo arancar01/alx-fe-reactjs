5d426e98f77473a2c698a29f5c45b3dcb5337bf7
hamza errahmouni <maradox6ken@gmail.com>
2025-03-24 05:01:34 +0000

    ............

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,16 +1,26 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://newsapi.org/v2';
+// GitHub API
+const GITHUB_BASE_URL = 'https://api.github.com/users';
 
-const apiKey = import.meta.env.VITE_REACT_APP_NEWS_API_KEY;
+export const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`${GITHUB_BASE_URL}/${username}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching GitHub user:', error);
+    throw error;
+  }
+};
+
+// News API
+const NEWS_BASE_URL = 'https://newsapi.org/v2';
+const newsApiKey = import.meta.env.VITE_REACT_APP_NEWS_API_KEY;
 
 export const fetchTopHeadlines = async (country = 'us') => {
   try {
-    const response = await axios.get(`${BASE_URL}/top-headlines`, {
-      params: {
-        country,
-        apiKey,
-      },
+    const response = await axios.get(`${NEWS_BASE_URL}/top-headlines`, {
+      params: { country, apiKey: newsApiKey },
     });
     return response.data.articles;
   } catch (error) {
@@ -21,11 +31,8 @@ export const fetchTopHeadlines = async (country = 'us') => {
 
 export const fetchNewsByQuery = async (query) => {
   try {
-    const response = await axios.get(`${BASE_URL}/everything`, {
-      params: {
-        q: query,
-        apiKey,
-      },
+    const response = await axios.get(`${NEWS_BASE_URL}/everything`, {
+      params: { q: query, apiKey: newsApiKey },
     });
     return response.data.articles;
   } catch (error) {
